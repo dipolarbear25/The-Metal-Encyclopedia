9cbe9ea11b742e583665e9ac6eaa9abc839932b3
Austin Mangum <mangumaustin@gmail.com>
2023-11-30 22:51:57 -0600

    read for songs is reached

--- components/songCards.js
+++ components/songCards.js
@@ -2,30 +2,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import Button from 'react-bootstrap/Button';
-import { Card, CardHeader } from 'react-bootstrap/Card';
-import { CardBody } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 // import Link from 'next/link';
 // import Image from 'next/image';
-// import { deleteAlbum } from '../api/albumApi';
+import { deleteSong } from '../api/songApi';
 
-function SongCard({ Obj }) {
-  // const deleteThisSong = () => {
-  //   if (window.confirm(`Would you like to delete ${Obj.title}?`)) {
-  //     deleteAlbum(Obj.firebaseKey).then(() => onUpdate());
-  //   }
-  // };
+function SongCard({ Obj, onUpdate }) {
+  // eslint-disable-next-line no-unused-vars
+  const deleteThisSong = () => {
+    if (window.confirm(`Would you like to delete ${Obj.title}?`)) {
+      deleteSong(Obj.firebaseKey).then(() => onUpdate());
+    }
+  };
 
   return (
     <Card>
-      <CardHeader>{Obj.title}</CardHeader>
-      <CardBody>
+      <Card.Header>{Obj.title}</Card.Header>
+      <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p> {Obj.lyrics} </p>
+          <p>
+            {' '}
+            {Obj.lyrics}{' '}
+          </p>
           <footer className="blockquote-footer">
             Someone famous in <cite title="Source Title">Source Title</cite>
           </footer>
         </blockquote>
-      </CardBody>
+      </Card.Body>
     </Card>
   );
 }
@@ -38,7 +41,7 @@ SongCard.propTypes = {
     title: PropTypes.string,
     firebaseKey: PropTypes.string,
   }).isRequired,
-  // onUpdate: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default SongCard;
